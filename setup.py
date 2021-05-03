0e829d1031dbbd44986755c90865f4f485b5a53a
Sæþór Hallgrímsson <saethor@hringdu.is>
2021-05-03 15:25:22 +0000

    chore: Pipenv

--- setup.py
+++ setup.py
@@ -3,19 +3,19 @@ from distutils.core import setup
 import kennitala
 
 setup(
-    name='kennitala',
+    name="kennitala",
     version=kennitala.__version__,
-    url='https://github.com/ojake/python-kennitala',
-    description='Icelandic national registry codes made easy',
-    author='Jakub Owczarski',
-    author_email='jakub3279@gmail.com',
-    packages=['kennitala', 'tests'],
-    license='MIT license',
-    long_description=open('README').read(),
+    url="https://github.com/ojake/python-kennitala",
+    description="Icelandic national registry codes made easy",
+    author="Jakub Owczarski",
+    author_email="jakub3279@gmail.com",
+    packages=["kennitala", "tests"],
+    license="MIT license",
+    long_description=open("README").read(),
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3'
-    ]
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
 )
